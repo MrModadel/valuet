5ca9bdd276ee04212dff22d65eb980ff6c4dc575
MrModadel <timanormu@gmail.com>
2023-06-22 14:13:37 +0300

    news

---
+++ main.js
@@ -0,0 +1,10 @@
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+// let swiper = new Swiper('.wallets__top-box-swiper .swiper', {
+//    spaceBetween: 24,
+//    slideToClickedSlide: true,
+//    slidesPerView: 2.5,
+//    freeMode:true, 
+// })
